01b14b6d637725d190f4fac0f73955d0c9aacbb7
shikunyaoa <shikunaaa@163.com>
2019-01-21 20:02:14 +0800

    add debugging

---
+++ debugging.js
@@ -0,0 +1,17 @@
+//use the javascript console to check the value of a variable
+let a = 5;
+let b = 1;
+a++;
+
+console.log(a);
+let sumAB = a + b;
+console.log(sumAB);
+
+
+//use typeof to check the type of a variable
+let seven = 7;
+let three = "3";
+console.log(typeof seven)
+console.log(typeof three)
+console.log(seven + three);
+
